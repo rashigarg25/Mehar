0030eb3d1f6ad2e9f93bc038b751971780a8e3cd
Sumit Jindal <sumit.jindal@sophos.com>
2022-09-29 12:58:47 +0530

    Fixed Header in PDF conversion

--- controller/homeController.js
+++ controller/homeController.js
@@ -5,7 +5,8 @@ const path = require('path');
 
 const printView = (req, res, next) => {
     var reqData = req.body;
-    res.render("print_report", {data : req.body});
+    console.log(">>>> Request body: " + JSON.parse(reqData));
+    res.render("print_report", {data: req.body});
 }
 
 const generatePdf = async (req, res, next) => {
@@ -21,21 +22,26 @@ const generatePdf = async (req, res, next) => {
     const options = {
         format: 'A4',
         orientation: 'portrait',
-        border: '50px'
+        base: "file:///home/www/",
+        border: '10px',
+        header: {
+            height: '220px'
         }
-    
+    }
+
     pdf.create(document, options)
         .then(res => {
             console.log(res);
         }).catch(error => {
-            console.log(error);
-        });
-        const filepath = 'http://localhost:3000/docs/' + filename;
+        console.log(error);
+    });
+    const filepath = 'http://localhost:3000/docs/' + filename;
 
-        res.render('print_report', {
-            path: filepath,
-            data: req.body
-        });
+    console.log(">>>>>>> Received body:\n" + JSON.stringify(req.body));
+    res.render('print_report', {
+        path: filepath,
+        data: req.body
+    });
 }
 
 module.exports = {
